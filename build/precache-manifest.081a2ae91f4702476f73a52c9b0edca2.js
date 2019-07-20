8d4cc4b28b39432f2f5287854e0a82daedef89de
cmmanik <mkmanik6523u@gmail.com>
2019-07-20 23:17:28 +0600

    init project

---
+++ build/precache-manifest.081a2ae91f4702476f73a52c9b0edca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c6ebeb17ebfe6270f56",
+    "url": "/static/js/main.7c6ebeb1.chunk.js"
+  },
+  {
+    "revision": "875ad03772843e90d573",
+    "url": "/static/js/1.875ad037.chunk.js"
+  },
+  {
+    "revision": "7c6ebeb17ebfe6270f56",
+    "url": "/static/css/main.50422aac.chunk.css"
+  },
+  {
+    "revision": "875ad03772843e90d573",
+    "url": "/static/css/1.cb15c1a5.chunk.css"
+  },
+  {
+    "revision": "707f32c296ab31cffe9723bca35314e1",
+    "url": "/index.html"
+  }
+];
